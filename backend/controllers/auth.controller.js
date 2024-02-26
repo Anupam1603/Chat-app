529aaf0c2bc18e0d414b2c3f918ba0f6153c5ea8
Anupam Tiwari <lonesamurai1603@gmail.com>
2024-02-26 10:50:19 +0530

    Added logout functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -87,8 +87,10 @@ export const login = async (req,res) => {
     }
 }
 export const logout =async (req,res) => {
+
     try {
-      res.send('logout page')
+       res.cookie("jwt", {maxAge:0})
+       res.status(200).json({message: "User logged out sucessfully"})
     } catch (error) {
       console.log("Error in logout controller", error.message);
       res.status(500).json({error:"Internal server error"})
